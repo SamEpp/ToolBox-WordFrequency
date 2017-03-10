523c6b4139a376640b2520e054056b74ef636387
Sam Eppinger <samantha.eppinger@students.olin.edu>
2017-03-09 21:15:47 -0500

    I am submitting the word frequency tool box

--- frequency.py
+++ frequency.py
@@ -1,19 +1,40 @@
 """ Analyzes the word frequencies in a book downloaded from
 Project Gutenberg """
 
+import requests
+import nltk
+import pickle
 import string
 
+Frankenstein_text = requests.get('http://www.gutenberg.org/ebooks/84.txt.utf-8').text
 
-def get_word_list(file_name):
+f = open('Frankenstein_text.pickle', 'wb')
+pickle.dump(Frankenstein_text, f)
+f.close()
+
+#Load data from a file (will be part of your data processing script)
+input_file = open('Frankenstein_text.pickle', 'rb')
+reloaded_copy_of_texts = pickle.load(input_file)
+
+def process_file(file_name):
     """ Reads the specified project Gutenberg book.  Header comments,
     punctuation, and whitespace are stripped away.  The function
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    file_name = file_name[200:]
+    hist = dict()
+    line = file_name.replace('-', ' ')
+    for word in line.split():
+        word = word.strip(string.punctuation + string.whitespace)
+        word = word.lower()
+        hist[word] = hist.get(word, 0) + 1
+    return hist
+
+hist = process_file(reloaded_copy_of_texts)
 
 
-def get_top_n_words(word_list, n):
+def get_top_n_words(word_list):
     """ Takes a list of words as input and returns a list of the n most frequently
     occurring words ordered from most to least frequently occurring.
 
@@ -23,8 +44,14 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    t = []
+    for key, value in hist.items():
+        t.append((value, key))
+    t.sort(reverse=True)
+    return t
+t = get_top_n_words(hist)
+for freq, word in t[:99]:
+    print(word,freq)
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
